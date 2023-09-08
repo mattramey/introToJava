3129ca264494dfa1dd203173eaba16b86b57defa
mattramey <110432461+mattramey@users.noreply.github.com>
2023-09-07 20:15:45 -0500

    initial commit

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to computer Science");
+        System.out.println("Programming is fun");
+    }
+}
+  
---
+++ src/ForLoop.java
@@ -0,0 +1,14 @@
+public class ForLoop {
+    /* A forloop will be written as follows:
+     * for (variable; condition; change)
+     * statment
+     */
+    /* If a for loop has multiple statements: 
+     * curly brackets will be added after parenthesis
+     * for (variable; condition; change;){
+     * }
+     */
+for(int i = 1; i <= 5; i++){
+    System.out.println("whats up");
+}
+}
